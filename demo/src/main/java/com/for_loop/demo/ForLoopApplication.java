3bd0b17e3fd78532f45eb3f831213077889db64d
Gerard Garro <geraardgarro@gmail.com>
2026-02-25 09:49:52 +0100

    Primer commit, Producto 1

---
+++ demo/src/main/java/com/for_loop/demo/ForLoopApplication.java
@@ -0,0 +1,13 @@
+package com.for_loop.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForLoopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForLoopApplication.class, args);
+	}
+
+}
