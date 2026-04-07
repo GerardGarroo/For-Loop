47c1fed2eca97992d1634c41ee5d04d9557204e1
Gerard Garro <geraardgarro@gmail.com>
2026-04-07 11:22:36 +0200

    Fix: compilation errors for terminal run

--- Producto 3/demo/src/main/java/com/for_loop/alquilatusvehiculos/config/SecurityConfig.java
+++ Producto 3/demo/src/main/java/com/for_loop/alquilatusvehiculos/config/SecurityConfig.java
@@ -10,7 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
 
 @Configuration
 @EnableWebSecurity
@@ -36,7 +36,7 @@ public class SecurityConfig {
                 .permitAll()
             )
             .logout(logout -> logout
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutUrl("/logout")
                 .logoutSuccessUrl("/")
                 .permitAll()
             );
